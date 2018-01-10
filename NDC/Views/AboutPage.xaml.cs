ea5aeb9b0fb3ce685c571bad559411e744887bcc
Bianca Minnaar <bminnaar@gmail.com>
2018-01-10 14:38:22 +0200

    Initial Commit

---
+++ NDC/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace NDC
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
