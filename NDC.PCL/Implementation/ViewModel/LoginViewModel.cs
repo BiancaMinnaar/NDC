a8198e9cd4c170ad44788b85002c748a4eeb4d32
Bianca Minnaar <bminnaar@gmail.com>
2018-01-15 09:51:42 +0200

    Move Login to user folder

---
+++ NDC.PCL/Implementation/ViewModel/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NDC.Implementation.ViewModel.Base;
+
+namespace NDC.PCL.Implementation.ViewModel
+{
+    public class LoginViewModel : NDCBaseViewModel
+    {
+        public string UserName { get; set; }
+        private string _Password;
+        public string Password
+        {
+            get
+            {
+                string encrypt = Encrypt(_Password);
+                return encrypt;
+            }
+            set { _Password = value; }
+        }
+
+        public DateTime DateLastChanged { get; set; }
+    }
+}
