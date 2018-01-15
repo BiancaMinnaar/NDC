a0f4c406b9cb8d429107a545ba548310711dc570
Bianca Minnaar <bminnaar@gmail.com>
2018-01-15 13:53:46 +0200

    Fix ios - Add interfacing - Add Constants.

---
+++ NDC.PCL/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NDC.PCL
+{
+    public static class Constants
+    {
+        public static string APPLICATION_TYPE = "application/json";
+        // URL of REST service
+        // public static string BASE_URL = "http://ww-apps.ddns.net:1775";
+        public static string BASE_URL = "http://10.0.0.242:1775";
+        //   public static string BASE_URL = "http://10.0.0.242:1768";
+        public static int SCREEN_AVAILABLE_HEIGHT = 100;
+    }
+}
