a11b50303ffe8ed10b505fa8d585c4cf7ce0baf9
Bianca Minnaar <bminnaar@gmail.com>
2018-01-16 13:23:01 +0200

    Start Templating project - Add ViewTemplate.

---
+++ SourceConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SourceConsole.Templates;
+
+namespace SourceConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ViewT tt = new ViewT();
+            string fileOut = tt.TransformText();
+            System.IO.File.WriteAllText("View.cs", fileOut);  
+        }
+    }
+}
