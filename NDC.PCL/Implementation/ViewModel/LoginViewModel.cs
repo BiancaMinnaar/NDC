17a4c2551eecb7edef04158f52a03319c2966f90
Bianca Minnaar <bminnaar@gmail.com>
2018-01-22 15:00:01 +0200

    Cleanup Login Model

--- NDC.PCL/Implementation/ViewModel/LoginViewModel.cs
+++ NDC.PCL/Implementation/ViewModel/LoginViewModel.cs
@@ -6,18 +6,6 @@ namespace NDC.Implementation.ViewModel
     public class LoginViewModel : ProjectBaseViewModel
     {
         public string UserName { get; set; }
-        //private string _Password;
-        //public string Password 
-        //{ 
-        //    get 
-        //    { 
-        //        string encrypt = Encrypt(_Password);
-        //        return encrypt;
-        //    } 
-        //    set { _Password = value; }
-        //}
         public string Password { get; set; }
-
-        public DateTime DateLastChanged { get; set; }
     }
 }
