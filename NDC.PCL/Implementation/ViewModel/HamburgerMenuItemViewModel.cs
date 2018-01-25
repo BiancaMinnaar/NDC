4c946231860964cd2fa8da642cb36b0c5e9e622e
Bianca Minnaar <bminnaar@gmail.com>
2018-01-25 18:18:00 +0200

    Further Menu - Add GestureRecognizer To Menu - Add MenuItem Model Properties.

--- NDC.PCL/Implementation/ViewModel/HamburgerMenuItemViewModel.cs
+++ NDC.PCL/Implementation/ViewModel/HamburgerMenuItemViewModel.cs
@@ -1,10 +1,19 @@
 using System;
+using System.Windows.Input;
 using NDC.Base;
 
 namespace NDC.Implementation.ViewModel
 {
     public class HamburgerMenuItemViewModel : ProjectBaseViewModel
     {
-        
+        public string ListIndex { get; set; }
+        public string IndexIndicator { get; set; }
+        public string Description { get; set; }
+
+        ICommand menuItemClicked;
+        public ICommand MenuItemClickedCommand
+        {
+            get { return menuItemClicked; }
+        }
     }
 }
